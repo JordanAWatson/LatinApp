3942df35b73f326d28dfe099e86bfcba8cdfbf5a
JordanAWatson <JordanA.Watson@calbaptist.edu>
2021-10-23 00:45:28 -0700

    Database update and more functions to Dictionary
    seting the char types to unicode didn't fix the sql comparison issue.

--- New Version/Latin_db.sql
+++ New Version/Latin_db.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS verbForm;
 DROP TABLE IF EXISTS dictionary;
 
 CREATE TABLE dictionary (
-	verb VARCHAR(28),
+	verb VARCHAR(28) CHARACTER SET UTF8MB4,
     conjugation VARCHAR(6),			/* 1st, 2nd, 3rd, 4th, 3rd-io */
     PRIMARY KEY (verb)
 );
 
 CREATE TABLE verbForm (
-	form VARCHAR(28),			/* The conjugated verb form */
-    stem VARCHAR(28),			/* Its first principle part */
+	form VARCHAR(28) CHARACTER SET UTF8MB4,			/* The conjugated verb form */
+    stem VARCHAR(28) CHARACTER SET UTF8MB4,			/* Its first principle part */
     PRIMARY KEY (form),
     FOREIGN KEY (stem) REFERENCES dictionary(verb)
     ON UPDATE CASCADE
@@ -19,7 +19,7 @@ CREATE TABLE verbForm (
 );
 
 CREATE TABLE formInfo (
-	form VARCHAR(28),
+	form VARCHAR(28) CHARACTER SET UTF8MB4,
 	voice CHAR(3),				/* ACT, PAS */
     mood CHAR(3), 				/* IND, SUB, IMP, INF */
     tense CHAR(4),				/* PRES, IMPF, FUTR, PERF, PLPF, FRPF */
